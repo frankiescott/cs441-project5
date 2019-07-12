295b32b2aef76456889869f48909e40d500df65f
Frankie <email@frankiescott.net>
2019-07-11 21:38:43 -0400

    added handlers for widgets

--- app/src/main/java/edu/binghamton/project5/MainActivity.java
+++ app/src/main/java/edu/binghamton/project5/MainActivity.java
@@ -2,12 +2,24 @@ package edu.binghamton.project5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button add, clear;
+    private EditText input;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = findViewById(R.id.add);
+        clear = findViewById(R.id.clear);
+        input = findViewById(R.id.input);
+        list = findViewById(R.id.list);
     }
 }

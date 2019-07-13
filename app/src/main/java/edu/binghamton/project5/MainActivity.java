ef99906456af199fedcc2e7f6865cb383f049a94
Frankie <email@frankiescott.net>
2019-07-13 11:13:53 -0400

    implemented array adapter and running total calculator

--- app/src/main/java/edu/binghamton/project5/MainActivity.java
+++ app/src/main/java/edu/binghamton/project5/MainActivity.java
@@ -2,15 +2,21 @@ package edu.binghamton.project5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button add, clear;
     private EditText input;
     private ListView list;
+    private TextView total;
+    int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
         clear = findViewById(R.id.clear);
         input = findViewById(R.id.input);
         list = findViewById(R.id.list);
+        total = findViewById(R.id.total);
+        ArrayList<String> arrayList = new ArrayList<String>();
+        for (int i: array) {
+            arrayList.add(String.valueOf(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, arrayList);
+        list.setAdapter(adapter);
+
+        calculateTotal();
+    }
+
+    public void calculateTotal() {
+        int totalCount = 0;
+        for (int i: array) {
+            totalCount += i;
+        }
+        total.setText(String.valueOf(totalCount));
     }
 }

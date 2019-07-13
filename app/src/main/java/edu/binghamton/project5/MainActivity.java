399357f729239e999b51ec083f5bb311fb1ae6e5
Frankie <email@frankiescott.net>
2019-07-13 13:38:08 -0400

    converted listview to recyclerview

--- app/src/main/java/edu/binghamton/project5/MainActivity.java
+++ app/src/main/java/edu/binghamton/project5/MainActivity.java
@@ -2,11 +2,11 @@ package edu.binghamton.project5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,10 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
     private Button add, clear;
     private EditText input;
-    private ListView list;
+    private RecyclerView list;
     private TextView total;
+
     private ArrayList<String> arrayList;
-    private ArrayAdapter<String> adapter;
+    private RecyclerAdapter adapter;
+    private RecyclerView recyclerView;
 
     //this will be used to load values upon application startup but afterwards use the ArrayList
     int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
@@ -39,8 +41,11 @@ public class MainActivity extends AppCompatActivity {
             arrayList.add(String.valueOf(i));
         }
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, arrayList);
-        list.setAdapter(adapter);
+        recyclerView = findViewById(R.id.list);
+        adapter = new RecyclerAdapter(this, arrayList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.refreshDrawableState();
 
         calculateTotal();
         configureClearButton();

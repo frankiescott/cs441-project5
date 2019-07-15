4b214946617eecba5579910b79b8adbd801cd7da
Frankie <email@frankiescott.net>
2019-07-15 16:00:15 -0400

    app now supports loading and saving data

--- app/src/main/java/edu/binghamton/project5/MainActivity.java
+++ app/src/main/java/edu/binghamton/project5/MainActivity.java
@@ -11,9 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements ListChanged {
+    private String filename;
+
     private Button add, clear;
     private EditText input;
     private RecyclerView list;
@@ -24,21 +30,27 @@ public class MainActivity extends AppCompatActivity implements ListChanged {
     private RecyclerView recyclerView;
 
     //this will be used to load values upon application startup but afterwards use the ArrayList
-    int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+    ArrayList<Integer> array = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        filename = getFilesDir().getPath() + "/data.txt";
+        try {
+            loadData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         add = findViewById(R.id.add);
         clear = findViewById(R.id.clear);
         input = findViewById(R.id.input);
-        list = findViewById(R.id.list);
         total = findViewById(R.id.total);
 
-        arrayList = new ArrayList<String>();
-        for (int i: array) {
+        arrayList = new ArrayList<>();
+        for (Integer i: array) {
             arrayList.add(String.valueOf(i));
         }
 
@@ -80,12 +92,23 @@ public class MainActivity extends AppCompatActivity implements ListChanged {
             totalCount += Integer.parseInt(i);
         }
         total.setText(String.valueOf(totalCount));
+
+        try {
+            saveData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void clear() {
         arrayList.clear();
         adapter.notifyDataSetChanged();
         total.setText("0");
+        try {
+            saveData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void add() {
@@ -94,5 +117,43 @@ public class MainActivity extends AppCompatActivity implements ListChanged {
         adapter.notifyDataSetChanged();
         calculateTotal();
         input.setText("");
+    }
+
+    public void saveData() throws IOException {
+        File f = new File(filename);
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+
+        FileOutputStream fout = openFileOutput("data.txt", MODE_PRIVATE);
+        String out = "";
+        for (String i: arrayList) {
+            out = out + i + " ";
+        }
+        System.out.println("Saving Data: " + out);
+        fout.write(out.getBytes());
+        fout.close();
+    }
+
+    public void loadData() throws IOException {
+        File f = new File(filename);
+        if (!f.exists()) {
+            f.createNewFile();
+            return;
+        }
+        if (f.length() == 0) {
+            return;
+        }
+
+        FileInputStream fin = new FileInputStream(new File(filename));
+        int c;
+        String data = "";
+        while((c = fin.read()) != -1) {
+            data = data + Character.toString((char) c);
+        }
+        String[] splitData = data.split(" ");
+        for (String i: splitData) {
+            array.add(Integer.valueOf(i));
+        }
     }
 }
